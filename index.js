09790886df63ff88974ef4fc69a615346b027bab
Jacob Mischka <jacob@mischka.me>
2019-05-09 16:07:50 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+/** @format */
+
+const jsonBody = require('body/json');
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+const ALLOWED_ORIGINS = [
+	/\.residentprogram\.com/,
+	/\.mcw-anesthesiology\.tech/,
+	/\.mcw-anesth\.tech/,
+	/\.mcwanet\.com/
+];
+
+module.exports = (req, res) =>
+	jsonBody(req, res, async (err, { body, styles = [] }) => {
+		try {
+			if (err) throw err;
+
+			if (ALLOWED_ORIGINS.some(origin => origin.test(req.headers.origin))) {
+				res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+			} else {
+				throw new Error('Disallowed origin');
+			}
+
+			const browser = await puppeteer.launch({
+				args: chrome.args,
+				executablePath: await chrome.executablePath,
+				headless: chrome.headless
+			});
+
+			const page = await browser.newPage();
+			await page.setContent(body);
+			await Promise.all(
+				styles.map(style =>
+					page.addStyleTag(style).catch(err => {
+						console.error(err);
+					})
+				)
+			);
+			const pdf = await page.pdf({
+				format: 'letter',
+				landscape: true
+			});
+
+			await browser.close();
+
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/pdf');
+			res.end(pdf);
+		} catch (err) {
+			console.error(err);
+			res.statusCode = 500;
+			res.end();
+			return;
+		}
+	});
